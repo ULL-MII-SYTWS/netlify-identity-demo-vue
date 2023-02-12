2f54215cd223b35c7f34889861a87e70f351e207
Casiano Rodriguez-Leon <crguezl@ull.edu.es>
2023-02-12 10:54:03 +0000

    learning vue and netlify and the identity service and the netlify-identity-widget

---
+++ src/components/Public2.vue
@@ -0,0 +1,12 @@
+<template>
+    <div>
+    <h2>Second Public Page</h2>
+    You can have more than  public page ...
+    </div>
+</template>
+
+<script>
+    export default {
+        name:"SecondPublic"
+    }
+</script>
